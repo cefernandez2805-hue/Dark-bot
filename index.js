fc1ce19f978b3eb7ba4a887c1087726a64596393
cefernandez2805-hue <cefernandez2805@gmail.com>
2026-08-27 22:08:55 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Dark-bot está vivo!'));
+app.listen(port, () => console.log(`Servidor web activo en puerto ${port}`));
+
 const { Client, GatewayIntentBits, Partials, EmbedBuilder } = require('discord.js');
 require('dotenv').config();
 
